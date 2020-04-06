9a02cb41406f9fcf9b649c13995c2b37dfd4c214
0x42424242 <34839743+hex0x42424242@users.noreply.github.com>
2020-04-06 14:27:26 +0000

    Final Version

--- kcv.py
+++ kcv.py
@@ -1,82 +1,34 @@
 #!/usr/bin/python3
-#Renato Hormazabal
 
 import binascii
-import hashlib
-import base64
-import urllib.request
-import requests
-import re
 import os
-import pandas
-import requests
-from bs4 import BeautifulSoup
-from time import sleep
 from pyDes import *
 
-os.urandom(24)    # 24*8=192 bits
-os.urandom(24).hex()
-key = os.urandom(24).hex()
+def calculate_combined_key(keys):
+    return "".join([
+        hex(int(ck1,16) ^ int(ck2,16) ^ int(ck3,16)).split('x')[1].upper() 
+        for ck1,ck2,ck3 in zip(keys[0],keys[1],keys[2])
+    ])
 
-print('Key: ' + str.upper(key))
+def generate_keys():
+    return tuple([os.urandom(24).hex().upper() for _ in range(3)])
 
-os.urandom(24)    # 24*8=192 bits
-os.urandom(24).hex()
-key2 = os.urandom(24).hex()
+def generate_kcv(key):
 
-print('Key2: ' + str.upper(key2))
+    validation_data = binascii.unhexlify('0' * 30)
+    
+    tdes_obj = triple_des(binascii.unhexlify(key),ECB,"\0\0\0\0\0\0\0\0",pad=None,padmode=PAD_PKCS5)
 
-os.urandom(24)    # 24*8=192 bits
-os.urandom(24).hex()
-key3 = os.urandom(24).hex()
+    return binascii.hexlify(tdes_obj.encrypt(validation_data)).decode('utf-8')
 
-print('Key3: ' + str.upper(key3))
+if __name__ == '__main__':
+    # Gera as 3 chaves de 56 bits
+    keys = generate_keys()
 
-one = key
-two = key2
-three = key3
+    # Gera combined key
+    combined_key = calculate_combined_key(keys)
 
-r = requests.get("https://emvlab.org/keyshares/?combined=&combined_kcv=&one={one}&one_kcv=&two={two}&two_kcv=&three={three}&three_kcv=&numcomp=three&parity=ignore&action=Combine".format(one=one, two=two, three=three))
+    for i,k in enumerate(keys):
+        print(f"Key {i}:        {k}    KCV: {generate_kcv(k)}")
 
-if r.status_code == 200:
-    print('Requisição bem sucedida!')
-    content = r.content
-    #print(content)
-    soup = BeautifulSoup(content, 'html.parser')
-    combined_key = soup.find(id='combined').getText()
-    print('Combined Key: ' + str.upper(combined_key))
-print('.........................................................')
-
-value = '000000000000000000000000000000'
-value = binascii.unhexlify(value)
-
-key = binascii.unhexlify(key)
-key2 = binascii.unhexlify(key2)
-key3 = binascii.unhexlify(key3)
-combined_key = binascii.unhexlify(combined_key)
-
-print('Componet 1: ' + str(key))
-print('Component 2: ' + str(key2))
-print('Component 3: ' + str(key3))
-print('Combined Key: ' + str(combined_key))
-print('................................................')
-
-k = triple_des(key, ECB, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
-k2 = triple_des(key2, ECB, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
-k3 = triple_des(key3, ECB, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
-combined = triple_des(combined_key, ECB, "\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
-
-data = k.encrypt(value)
-data2 = k2.encrypt(value)
-data3 = k3.encrypt(value)
-data4 = combined.encrypt(value)
-
-loading = 'Calculando KCV, aguarde...'
-sleep(0.6)
-print('KCV: Component 1: ' + str(binascii.hexlify(data)[0:6]))
-sleep(0.1)
-print('KCV2: Component 2: ' + str(binascii.hexlify(data2)[0:6]))
-sleep(0.1)
-print('KCV3: Component 3: ' + str(binascii.hexlify(data3)[0:6]))
-sleep(0.1)
-print('KCV Combined Key: ' + str(binascii.hexlify(data4)[0:6]))
+    print(f"Combined Key: {combined_key}    KCV: {generate_kcv(combined_key)}")
